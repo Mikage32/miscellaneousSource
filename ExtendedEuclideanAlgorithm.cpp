53d50b104e4aa1fa60a1a56eeae86fffce761f02
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2019-10-01 14:41:58 +0900

    Create ExtendedEuclideanAlgorithm.cpp

---
+++ ExtendedEuclideanAlgorithm.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+using namespace std;
+
+//ax + by = gcd(a, b) を満たすx, yの組合わせを返す
+pll eea(ll a, ll b){
+    if(a < b) swap(a, b);
+    int i = 1;
+    vector<pll> qr;
+    qr.push_back(pll(0, b));
+    qr.push_back(pll(a/b, a%b));
+    while(qr[i].second != 0){
+        qr.push_back(pll(qr[i-1].second/qr[i].second, qr[i-1].second%qr[i].second));
+        ++i;
+    }
+
+    ll x = 1 - qr[i].first;
+    ll y = 0;
+    --i;
+    while(i >= 0){
+        ll t = y;
+        ll s = x - qr[i].first;
+        x = t;
+        y = s;
+        --i;
+    }
+
+    return pll(x, y);
+}
+
+int main(){
+
+}
