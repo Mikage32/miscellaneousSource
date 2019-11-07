3bcc50513ff1b42a552f77e1778fb1d66cb0b9cd
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2019-11-07 18:28:30 +0900

    Update warshall_floyd.cpp

--- warshall_floyd.cpp
+++ warshall_floyd.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 //ワーシャルフロイド法で連続グラフの各ノード間の距離を求める.
 void warshallFloyd(int** graph, int n) {
-	for(int i = 0; i < n; ++i) {
-		for(int j = 0; j < n; ++j) {
-			for(int k = 0; k < n; ++k) {
-				graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k]);
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			if (graph[j][i] == -1) continue;
+			for (int k = 0; k < n; ++k) {
+				if (graph[i][k] == -1)continue;
+				else if (graph[j][k] == -1) graph[j][k] = graph[j][i] + graph[i][k];
+				else graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k]);
 			}
 		}
 	}
