e0f310696286ec8e27834b604033680b9d9492bc
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2019-12-10 09:55:10 +0900

    Update binaryPow.cpp

--- binaryPow.cpp
+++ binaryPow.cpp
@@ -2,20 +2,27 @@
 using namespace std;
 
 template<typename T>
-T binaryPow(T radix, T index, T mod){
-    radix %= mod;
-    while(T > 0){
-        radix *= (radix * (radix*(index&1)))%mod;
-        radix %= mod;
-        index = index >> 1;
-    }
+T binaryPow(T radix, T index, T mod) {
+	radix %= mod;
+	T rtv = 1;
+	while (index > 0) {
+		if (index & 1) {
+			rtv *= radix;
+			rtv %= mod;
+		}
 
-    return radix;
+		radix *= radix;
+		radix %= mod;
+
+		index = index >> 1;
+	}
+
+	return rtv;
 }
 
-int main(){
-    unsigned long long radix, index, mod;
-    cin >> radix, index, mod;
+int main() {
+	unsigned long long radix, index, mod;
+	cin >> radix >> index >> mod;
 
-    cout << binaryPow(radix, index, mod) << "\n";
+	cout << binaryPow(radix, index, mod) << "\n";
 }
