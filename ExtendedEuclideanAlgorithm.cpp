8e5e672de39d376662417c860ce50de908bb1684
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2019-12-12 08:49:57 +0900

    Update ExtendedEuclideanAlgorithm.cpp

--- ExtendedEuclideanAlgorithm.cpp
+++ ExtendedEuclideanAlgorithm.cpp
@@ -3,34 +3,22 @@
 
 using namespace std;
 
-//ax + by = gcd(a, b) を満たす整数x, yの組合わせを返す. Extended Euclidean Algorithm
-//a = bq + r であるとき, b(qx+ y) + rx = gcd(a, b)であることを利用する.
-//なんとなくtemplate使ってみたけど整数型じゃないとバグるし微妙
+//ax + by = gcd(a, b) を満たす整数x, yの組合わせを返す. 
+//Extended Euclidean Algorithm
 template<typename T>
 pair<T, T> eea(T a, T b) {
-	int i = 1;
+	T x = 1, y = 0, p = 0, q = 1;
+	while(b != 0){
+		T s = a;
+		a = b;
+		T t = s/b;
+		b = s%b;
 
-	//q, rの組を, r = 0になるまで求め続ける.
-	//Ri = BQ(i+1) + R(i+1) 即ち, R(i+1) = R(i-1) % Ri, Q(i+1) = R(i-1) / Ri である.
-	vector<pair<T, T>> qr;
-	qr.push_back(pair<T, T>(0, b));
-	qr.push_back(pair<T, T>(a / b, a % b));
-	while (qr[i].second != 0) {
-		qr.push_back(pair<T, T>(qr[i - 1].second / qr[i].second, qr[i - 1].second % qr[i].second));
-		++i;
-	}
-
-	//Xi = Y(i+1)
-	//Yi = X(i-1) - Q(i-1)*Xi
-	//上記式を使ってX0, Y0を求める
-	T x = 1;
-	T y = 0;
-	while (i > 0) {
-		T t = y;
-		T s = x - qr[i].first * y;
-		x = t;
-		y = s;
-		--i;
+		T i = x, j = y;
+		x = p;
+		y = q;
+		p = i - t*p;
+		q = j - t*q;
 	}
 
 	return pair<T, T>(x, y);
