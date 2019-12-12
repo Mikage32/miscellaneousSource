e17af85f3403b5c2bde647b7cab40857b21d96d9
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2019-12-12 16:22:45 +0900

    Update ExtendedEuclideanAlgorithm.cpp

--- ExtendedEuclideanAlgorithm.cpp
+++ ExtendedEuclideanAlgorithm.cpp
@@ -8,17 +8,17 @@ using namespace std;
 template<typename T>
 pair<T, T> eea(T a, T b) {
 	T x = 1, y = 0, p = 0, q = 1;
-	while(b != 0){
+	while (b != 0) {
 		T s = a;
 		a = b;
-		T t = s/b;
-		b = s%b;
+		T t = s / b;
+		b = s % b;
 
 		T i = x, j = y;
 		x = p;
 		y = q;
-		p = i - t*p;
-		q = j - t*q;
+		p = i - t * p;
+		q = j - t * q;
 	}
 
 	return pair<T, T>(x, y);
