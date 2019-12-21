9a901c6839dc42ff781ad7a716f3c7f8eaf59fcf
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2019-12-22 02:29:30 +0900

    Update Find_divisors.cpp

--- Find_divisors.cpp
+++ Find_divisors.cpp
@@ -1,57 +1,32 @@
-/*
-written by Mikage32
-This is a program that uses the "findDivisor" function to calculate and return divisors
-after factoring the number (long long) received as an argument.
-*/
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <chrono>
 
-struct primeFactor{
-    long long prime;
-    long long power;
-};
+template<typename T>
+constexpr std::vector<std::pair<T, T>> primeFactorization(T number) noexcept {
+	std::vector<std::pair<T, T>> primeFactors;
+	T quotient = number;
+	for (T i = 2; i * i <= quotient; ++i) {
+		if (quotient % i == 0) {
+			T prime = i, power = 1;
 
-std::vector<primeFactor> primeFactorization(long long number){
-    std::vector<primeFactor> primeFactors;
-    
-    if(number == 0){
-        primeFactor pf;
-        pf.prime = 0;
-        pf.power = 1;
-        primeFactors.push_back(pf);
-        return primeFactors;
-    }
+			quotient /= i;
+			while (quotient % i == 0) {
+				++power;
+				quotient /= i;
+			}
 
-    long long quotient = number;
-    for(long long i = 2; i*i <= quotient; ++i){
-        if(quotient%i == 0){
-            primeFactor pf;
-            pf.prime = i;
-            pf.power = 1;
+			primeFactors.push_back(std::pair<T, T>(prime, power));
+		}
+	}
+	if (quotient != 1) primeFactors.push_back(std::pair<T, T>(quotient, 1));
 
-            quotient /= i;
-            while(quotient%i == 0){
-                ++pf.power;
-                quotient /= i;
-            }
-
-            primeFactors.push_back(pf);
-        }
-    }
-    if(quotient != 1){
-        primeFactor pf;
-        pf.prime = quotient;
-        pf.power = 1;
-        primeFactors.push_back(pf);
-    }
-
-    return primeFactors;
+	return primeFactors;
 }
 
-int countDivisor(std::vector<primeFactor> primeFactors){
+template<typename T>
+constexpr int countDivisor(std::vector<std::pair<T, T>> primeFactors) noexcept {
     int counter = 1;
     for(int i = 0; i < primeFactors.size(); ++i){
         counter *= (primeFactors[i].power + 1);
@@ -60,17 +35,18 @@ int countDivisor(std::vector<primeFactor> primeFactors){
     return counter;
 }
 
-std::vector<long long> findDivisor(long long number){
-    std::vector<primeFactor> primeFactors = primeFactorization(number);
+template<typename T>
+constexpr std::vector<T> findDivisor(T number) noexcept {
+    std::vector<std::pair<T, T>> primeFactors = primeFactorization(number);
 
-    std::vector<long long> divisor(countDivisor(primeFactors));
+    std::vector<T> divisor(countDivisor(primeFactors));
     divisor[0] = 1;
 
     int counter = 1;
     for(int i = 0; i < primeFactors.size(); ++i){
         int currentSize = counter;
         for(int j = 1; j <= primeFactors[i].power; ++j){
-            long long x = 1;
+            T x = 1;
             for(int k = 0; k < j; ++k){
                 x *= primeFactors[i].prime;
             }
