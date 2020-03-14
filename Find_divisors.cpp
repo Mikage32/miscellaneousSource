0ffab03b8007a686996e743dfedf2018f97c7827
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2020-03-14 20:48:47 +0900

    Update Find_divisors.cpp

--- Find_divisors.cpp
+++ Find_divisors.cpp
@@ -27,39 +27,39 @@ constexpr std::vector<std::pair<T, T>> primeFactorization(T number) noexcept {
 
 template<typename T>
 constexpr int countDivisor(std::vector<std::pair<T, T>> primeFactors) noexcept {
-    int counter = 1;
-    for(int i = 0; i < primeFactors.size(); ++i){
-        counter *= (primeFactors[i].power + 1);
-    }
+	int counter = 1;
+	for (int i = 0; i < primeFactors.size(); ++i) {
+		counter *= (primeFactors[i].second + 1);
+	}
 
-    return counter;
+	return counter;
 }
 
 template<typename T>
 constexpr std::vector<T> findDivisor(T number) noexcept {
-    std::vector<std::pair<T, T>> primeFactors = primeFactorization(number);
+	std::vector<std::pair<T, T>> primeFactors = primeFactorization(number);
 
-    std::vector<T> divisor(countDivisor(primeFactors));
-    divisor[0] = 1;
+	std::vector<T> divisor(countDivisor(primeFactors));
+	divisor[0] = 1;
 
-    int counter = 1;
-    for(int i = 0; i < primeFactors.size(); ++i){
-        int currentSize = counter;
-        for(int j = 1; j <= primeFactors[i].power; ++j){
-            T x = 1;
-            for(int k = 0; k < j; ++k){
-                x *= primeFactors[i].prime;
-            }
+	int counter = 1;
+	for (int i = 0; i < primeFactors.size(); ++i) {
+		int currentSize = counter;
+		for (int j = 1; j <= primeFactors[i].second; ++j) {
+			T x = 1;
+			for (int k = 0; k < j; ++k) {
+				x *= primeFactors[i].first;
+			}
 
-            for(int k = 0; k < currentSize; ++k){
-                divisor[counter + k] = divisor[k] * x;
-            }
-            counter += currentSize;
-        }
-    }
-    sort(divisor.begin(), divisor.end());
+			for (int k = 0; k < currentSize; ++k) {
+				divisor[counter + k] = divisor[k] * x;
+			}
+			counter += currentSize;
+		}
+	}
+	sort(divisor.begin(), divisor.end());
 
-    return divisor;
+	return divisor;
 }
 
 int main() {
